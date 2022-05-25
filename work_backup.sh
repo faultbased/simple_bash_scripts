fb6c967307c6e79f775fc071a75b7d22e8a80ba2
s.dated <s.dated@hotmail.com>
2022-05-25 23:12:05 +0000

    remove deleted files

--- work_backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "creating work backup log" >> ~/vagrant/nest/simple_bash_scripts/backup_logs && mkdir ~/vagrant/nest/work_backup
-
-echo "copying files" >> ~/vagrant/nest/simple_bash_scripts/backup_logs && cp -v ~/vagrant/nest/simple_bash_scripts/work/* ~/vagrant/nest/work_backup/
-
-echo "operation completed" >> ~/vagrant/nest/simple_bash_scripts/backup_logs
