93911d68890624b85c09acbb91c39b00dce2999c
s.dated <s.dated@hotmail.com>
2022-05-25 17:38:01 +0000

    completed lesson 3.3

--- backup.sh
+++ backup.sh
@@ -2,10 +2,26 @@
 
 #backing up req. files:
 
-echo "create backup dir" && mkdir ~/backup 2> /dev/null || echo "dir already exist!"
+BACKUP_LOC="/usr/bin/*"
+BACKUP_TARGET="/home/$USER/backup-variables"
+LOGFILE="/home/vagrant/log_file"
 
-echo "copy files" && cp -v /usr/bin/* ~/backup > log_file 2>&1
+#calling an undeclared function
+init () {
+	echo "create backup dir" && mkdir $BACKUP_TARGET 2> /dev/null || echo "dir already exist!"
+	#basically purges the log_file since it's never deleted
+	echo "" > $LOGFILE
+	#declares script local variable using 'local' parameter that is accessible to variable only 
+	local CLOUDSTUFF=36
+}
 
-grep -i denied log_file | tail -n 1
+#reassigning tail command to read input for specified number to count
+tail () {
+	command tail -n $1
+}
+
+echo "copy files" && cp -v $BACKUP_LOC $BACKUP_TARGET >> $LOGFILE  2>&1
+
+grep -i denied $LOGFILE | tail  2
 
 exit 127
